e5f5b560bb82230d362a9856b651421981b856f2
“umerkhan-99” <umerkhanofficial394@gmail.com>
2026-03-25 12:08:17 +0500

    Java Script

---
+++ task2.js
@@ -0,0 +1,30 @@
+let a = 5
+let b = 10
+let sum = a + b
+console.log("Out Put of a and b =" , sum)
+
+console.log((5 + 6) * 10);
+
+var x
+x = 10
+console.log(x * 2)
+
+console.log("John" + " " + "Doe");
+
+console.log(8-7)
+
+console.log(10/3)
+
+let p = 5/25*100
+console.log(`${p}%`)
+
+let T = 75
+let S = 45
+let Persentage = S / T * 100
+console.log(Persentage+"%")
+
+let currentyear = 2026
+let dateofbirth = 1992
+
+let age = currentyear - dateofbirth
+console.log(age)
